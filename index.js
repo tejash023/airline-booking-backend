409b2a6f4a3f6ddc76a04b9415490f50a2e052a8
Tejashwa Rajvardhan <keshu023@gmail.com>
2022-12-21 23:26:08 +0530

    server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 const express  = require('express');
+
+const app = express();
+
+
+app.listen(3000, function(err){
+  if(err){
+    console.log('Error in starting the server');
+  }
+
+  console.log('server is started');
+})
