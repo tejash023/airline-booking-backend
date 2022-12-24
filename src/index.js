8c1ff4e0c07e5ed1fed8bf582bd25ab6620c0949
Tejashwa Rajvardhan <keshu023@gmail.com>
2022-12-24 17:51:56 +0530

    setup mysql

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const { PORT } = require('./config/serverConfig');
+const bodyParser = require('body-parser')
 
 const setupAndStartServer = async () => {
 
   //create the express object
   const app = express();
+  
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({urlencoded:true}));
+
   app.listen(PORT, () => {
     console.log(`Server started at PORT: ${PORT}`);
   })
