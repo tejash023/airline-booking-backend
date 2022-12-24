f9d8fe8b1445b4b44ce14ba0c9b11d4965615e79
Tejashwa Rajvardhan <keshu023@gmail.com>
2022-12-24 17:01:46 +0530

    added env variabled

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
+const { PORT } = require('./config/serverConfig');
 
 const setupAndStartServer = async () => {
 
   //create the express object
   const app = express();
-  const PORT = 8080;
   app.listen(PORT, () => {
     console.log(`Server started at PORT: ${PORT}`);
   })
